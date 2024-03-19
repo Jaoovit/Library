25102255ab6cfddf1818379fa4e6e5497ccabf3e
João Oliveira <jaoovit@gmail.com>
2024-03-19 16:59:21 +0000

    add three books at the myLibrary

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const myLibrary = [];
 
 function Book(author, title, pages, style, read) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.style = style;
-  this.read = read;
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.style = style;
+    this.read = read;
 };
 
+const book1 = new Book('J.R.R. Tolkien', 'The Hobbit', '288', 'fantasy', 'yes')
+const book2 = new Book('George Orwell', '1984', '314', 'romance', 'no')
+const book3 = new Book('Adam SMith', 'The Wealth of Nations', '524', 'economy', 'no' )
+
 function addBookToLibrary() {
-  // do stuff here
+    // do stuff here
 }
+
+const myLibrary = [book1, book2, book3];
+
+console.log(myLibrary)
