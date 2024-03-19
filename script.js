53629b2e5e42803da41700c591e89f6c1f3fe301
João Oliveira <jaoovit@gmail.com>
2024-03-19 16:04:46 +0000

    create the Book constructor

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
-}
+function Book(author, title, pages, style, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.style = style;
+  this.read = read;
+};
 
 function addBookToLibrary() {
   // do stuff here
