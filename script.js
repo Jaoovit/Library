ffc8a434d50b93917022cbbeea1f6f0ec5a5ac49
João Oliveira <jaoovit@gmail.com>
2024-03-20 13:03:04 +0000

    delete read and change style to type

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-function Book(author, title, pages, style, read) {
+function Book(author, title, pages, type) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.style = style;
-    this.read = read;
+    this.type = type;
 };
 
-const book1 = new Book('J.R.R. Tolkien', 'The Hobbit', '288', 'Fantasy', 'Yes')
-const book2 = new Book('George Orwell', '1984', '314', 'Romance', 'No')
-const book3 = new Book('Adam Smith', 'The Wealth of Nations', '524', 'Economy', 'No' )
+const book1 = new Book('J.R.R. Tolkien', 'The Hobbit', '288', 'Fantasy')
+const book2 = new Book('George Orwell', '1984', '314', 'Romance')
+const book3 = new Book('Adam Smith', 'The Wealth of Nations', '524', 'Economy')
 
 const myLibrary = [book1, book2, book3];
 
 function addBookToLibrary() {
-    const book = new Book ('Aristotle', 'Physics', '420', 'Philosophy', 'No')
+    const book = new Book ('Aristotle', 'Physics', '420', 'Philosophy')
     myLibrary.push(book);
 }
 
@@ -23,17 +22,14 @@ addBookToLibrary()
 document.querySelector('.book-one .title').innerHTML = myLibrary[0].title
 document.querySelector('.book-one .author').innerHTML += myLibrary[0].author
 document.querySelector('.book-one .pages').innerHTML += myLibrary[0].pages
-document.querySelector('.book-one .style').innerHTML += myLibrary[0].style
-document.querySelector('.book-one .read').innerHTML += myLibrary[0].read
+document.querySelector('.book-one .type').innerHTML += myLibrary[0].type
 
 document.querySelector('.book-two .title').innerHTML = myLibrary[1].title
 document.querySelector('.book-two .author').innerHTML += myLibrary[1].author
 document.querySelector('.book-two .pages').innerHTML += myLibrary[1].pages
-document.querySelector('.book-two .style').innerHTML += myLibrary[1].style
-document.querySelector('.book-two .read').innerHTML += myLibrary[1].read
+document.querySelector('.book-two .type').innerHTML += myLibrary[1].type
 
 document.querySelector('.book-three .title').innerHTML = myLibrary[2].title
 document.querySelector('.book-three .author').innerHTML += myLibrary[2].author
 document.querySelector('.book-three .pages').innerHTML += myLibrary[2].pages
-document.querySelector('.book-three .style').innerHTML += myLibrary[2].style
-document.querySelector('.book-three .read').innerHTML += myLibrary[2].read
+document.querySelector('.book-three .type').innerHTML += myLibrary[2].type
