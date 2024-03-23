bbc3451c3d6f98f45b88f5f7d4c718ca1b135aa2
João Oliveira <jaoovit@gmail.com>
2024-03-23 13:07:57 +0000

    splite the function to re-render html content

--- script.js
+++ script.js
@@ -7,28 +7,24 @@ function Book(author, title, pages, type) {
     this.type = type;
 };
 
-const book1 = new Book('J.R.R. Tolkien', 'The Hobbit', '288', 'Fantasy')
-const book2 = new Book('George Orwell', '1984', '314', 'Dystopian')
-const book3 = new Book('Adam Smith', 'The Wealth of Nations', '524', 'Economy')
+const book1 = new Book('J.R.R. Tolkien', 'The Hobbit', '288', 'Fantasy');
+const book2 = new Book('George Orwell', '1984', '314', 'Dystopian');
+const book3 = new Book('Adam Smith', 'The Wealth of Nations', '524', 'Economy');
 
-myLibrary.push(book1)
-myLibrary.push(book2)
-myLibrary.push(book3)
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
 
 
-const menu = document.querySelector('.container')
-let bookAuthor = document.querySelector('#book-author')
-let bookTitle = document.querySelector('#book-title')
-let bookPages = document.querySelector('#book-pages')
-let bookType = document.querySelector('#book-type')
+const menu = document.querySelector('.container');
+let bookAuthor = document.querySelector('#book-author');
+let bookTitle = document.querySelector('#book-title');
+let bookPages = document.querySelector('#book-pages');
+let bookType = document.querySelector('#book-type');
 
-let sendButton = document.querySelector('button[type="submit"]')
+let sendButton = document.querySelector('button[type="submit"]');
 
-sendButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    const book = new Book (bookAuthor.value, bookTitle.value, bookPages.value, bookType.value)
-    myLibrary.push(book);
-
+function createBook(book) {
     let div = document.createElement('div')
     div.setAttribute('id', 'book')
 
@@ -52,70 +48,60 @@ sendButton.addEventListener('click', function(event) {
     delButton.setAttribute('class', 'delete')
     delButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>'
     
-    div.appendChild(title)
-    div.appendChild(author)
-    div.appendChild(pages)
-    div.appendChild(type)
-    div.appendChild(delButton)
-    menu.appendChild(div)
-    menu.insertBefore(div, document.querySelector('.book-add'))
+    div.appendChild(title);
+    div.appendChild(author);
+    div.appendChild(pages);
+    div.appendChild(type);
+    div.appendChild(delButton);
+    return div;
+}
+
+sendButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    const book = new Book (bookAuthor.value, bookTitle.value, bookPages.value, bookType.value);
+
+    myLibrary.push(book);
+
+    refreshBooks();
 
     document.querySelector('#popup-window').style.display = 'none';
     document.querySelector('.container').style = 'opacity: 100%';
-
-    let del = document.querySelectorAll('.delete')
-    let i;
-
-    for (i = 0; i < del.length; i++) {
-        del[i].onclick = function() {
-        let div = this.parentElement;
-        div.remove();
-    }
     
-}
+});
+
+function refreshBooks() {
+    console.log(myLibrary);
+
+    document.querySelectorAll('#book').forEach((book) => {
+        book.remove();
+    });
+
+
+myLibrary.forEach((book) => {
+    const div = createBook(book);
+    menu.appendChild(div);
+    menu.insertBefore(div, document.querySelector(".book-add"))
 })
 
-//book1
+let del = document.querySelectorAll('.delete')
+for (let i = 0; i < del.length; i++) {
+      del[i].onclick = function() {
+        myLibrary.splice(i, 1);
+        refreshBooks();
+      };
+    }
+}
 
-document.querySelector('.book-one .title').innerHTML = myLibrary[0].title
-document.querySelector('.book-one .author').innerHTML += myLibrary[0].author
-document.querySelector('.book-one .pages').innerHTML += myLibrary[0].pages
-document.querySelector('.book-one .type').innerHTML += myLibrary[0].type
-
-//book2
-
-document.querySelector('.book-two .title').innerHTML = myLibrary[1].title
-document.querySelector('.book-two .author').innerHTML += myLibrary[1].author
-document.querySelector('.book-two .pages').innerHTML += myLibrary[1].pages
-document.querySelector('.book-two .type').innerHTML += myLibrary[1].type
-
-//book3
-
-document.querySelector('.book-three .title').innerHTML = myLibrary[2].title
-document.querySelector('.book-three .author').innerHTML += myLibrary[2].author
-document.querySelector('.book-three .pages').innerHTML += myLibrary[2].pages
-document.querySelector('.book-three .type').innerHTML += myLibrary[2].type
+refreshBooks()
 
 //book-add-buttom
 
-document.querySelector('.open-forms').addEventListener ('click', function () {
-    document.querySelector('#popup-window').style.display = 'block';
-    document.querySelector('.container').style = 'opacity: 20%';
-});
-
-document.querySelector('#close-button').addEventListener('click', function() {
-    document.querySelector('#popup-window').style.display = 'none';
-    document.querySelector('.container').style = 'opacity: 100%';
-})
-;
-
-
-let del = document.querySelectorAll('.delete')
-let i;
-
-for (i = 0; i < del.length; i++) {
-    del[i].onclick = function() {
-        let div = this.parentElement;
-        div.remove();
-    }
-}
+document.querySelector(".open-forms").addEventListener("click", function () {
+    document.querySelector("#popup-window").style.display = "block";
+    document.querySelector(".container").style = "opacity: 20%";
+  });
+  
+  document.querySelector("#close-button").addEventListener("click", function () {
+    document.querySelector("#popup-window").style.display = "none";
+    document.querySelector(".container").style = "opacity: 100%";
+  });
